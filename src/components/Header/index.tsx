fc1a34dd49cb18fbf2dd2e586c1af0f0cd85a227
Sameer <samirsaini99@gmail.com>
2023-02-24 17:02:09 +0530

    Nav button add

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,43 +22,55 @@ const Header = () => {
     }, []);
 
     return (
-        <header
-            id='header'
-            className={isScrolled ? 'fixed-top header-scrolled' : 'fixed-top'}
-        >
-            <div className='container d-flex justify-content-between align-items-center'>
-                <p className='logo mr-auto d-flex align-items-center'>
-                    <img src={Logo} alt='Conwell Logo' className='img-fluid' />
-                    <a
-                        className='d-none d-md-block logo ps-3'
-                        href='index.html'
-                    >
-                        Conwell
+        <>
+            <button type='button' className='mobile-nav-toggle d-lg-none'>
+                <i className='icofont-navigation-menu'></i>
+            </button>
+            <div className='mobile-nav-overly'></div>
+            <header
+                id='header'
+                className={
+                    isScrolled ? 'fixed-top header-scrolled' : 'fixed-top'
+                }
+            >
+                <div className='container d-flex justify-content-between align-items-center'>
+                    <p className='logo mr-auto d-flex align-items-center'>
+                        <img
+                            src={Logo}
+                            alt='Conwell Logo'
+                            className='img-fluid'
+                        />
+                        <a
+                            className='d-none d-md-block logo ps-3'
+                            href='index.html'
+                        >
+                            Conwell
+                        </a>
+                    </p>
+
+                    <nav className='nav-menu d-none d-lg-block'>
+                        <ul>
+                            <li className='active'>
+                                <a href='index.html'>Home</a>
+                            </li>
+                            <li>
+                                <a href='#about'>About</a>
+                            </li>
+                            <li>
+                                <a href='#services'>Products</a>
+                            </li>
+                            <li>
+                                <a href='#contact'>Contact</a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <a href='#about' className='get-started-btn scrollto'>
+                        Get Started
                     </a>
-                </p>
-
-                <nav className='nav-menu d-none d-lg-block'>
-                    <ul>
-                        <li className='active'>
-                            <a href='index.html'>Home</a>
-                        </li>
-                        <li>
-                            <a href='#about'>About</a>
-                        </li>
-                        <li>
-                            <a href='#services'>Products</a>
-                        </li>
-                        <li>
-                            <a href='#contact'>Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-
-                <a href='#about' className='get-started-btn scrollto'>
-                    Get Started
-                </a>
-            </div>
-        </header>
+                </div>
+            </header>
+        </>
     );
 };
 
