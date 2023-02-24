0e96e1cc4e4c4390634ff054af45b535706b31b4
Sameer <samirsaini99@gmail.com>
2023-02-24 11:17:12 +0530

    Color scheme change and favicon added

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,10 +6,10 @@ const Hero = () => {
             <div className='container'>
                 <div className='row'>
                     <div className='col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1'>
-                        <h1>Better Solutions For Your Business</h1>
+                        <h1>Online Solution For Your Medical Needs</h1>
                         <h2>
-                            We are team of talanted designers making websites
-                            with Bootstrap
+                            We at Conwell aim to deliver the best healthcare
+                            products and related services.
                         </h2>
                         <div className='d-lg-flex'>
                             <a
