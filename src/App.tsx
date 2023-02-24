0fa2ac9295c6a2ceefbf38eb129cc5b5609fb366
Sameer <samirsaini99@gmail.com>
2023-02-24 12:46:14 +0530

    Faq Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import About from './components/About';
 import Call from './components/Call';
+import Faq from './components/Faq';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
@@ -14,6 +15,7 @@ const App = () => {
             <WhyUs />
             <Services />
             <Call />
+            <Faq />
         </>
     );
 };
