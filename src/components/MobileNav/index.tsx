7c699d1b3235081ad556dc5f65ddeaae0ecafc95
Sameer Saini <samirsaini99@gmail.com>
2023-02-25 19:45:57 +0530

    Mobile Nav links fixed

--- src/components/MobileNav/index.tsx
+++ src/components/MobileNav/index.tsx
@@ -2,6 +2,8 @@ const MobileNav = (props: {
     isVisible: boolean;
     toggleHandler: () => void;
 }) => {
+    const url = window.location.href.split('/').at(-1);
+
     return (
         <nav
             className={`${
@@ -9,22 +11,22 @@ const MobileNav = (props: {
             } mobile-nav d-lg-none`}
         >
             <ul>
-                <li className='active'>
+                <li className={url === '#home' ? 'active' : ''}>
                     <a onClick={props.toggleHandler} href='#home'>
                         Home
                     </a>
                 </li>
-                <li>
+                <li className={url === '#about' ? 'active' : ''}>
                     <a onClick={props.toggleHandler} href='#about'>
                         About
                     </a>
                 </li>
-                <li>
+                <li className={url === '#products' ? 'active' : ''}>
                     <a onClick={props.toggleHandler} href='#products'>
                         Products
                     </a>
                 </li>
-                <li>
+                <li className={url === '#contactus' ? 'active' : ''}>
                     <a onClick={props.toggleHandler} href='#contactus '>
                         Contact
                     </a>
